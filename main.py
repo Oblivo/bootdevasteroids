b037f90ff7b14719541fad7d6e430f9d6d698bf2
Oblivo <henrik.kiefer@googlemail.com>
2024-11-28 05:29:46 +0100

    new file:   asteroid.py 	new file:   asteroidfield.py 	new file:   circleshape.py 	modified:   constants.py 	modified:   main.py 	new file:   player.py

--- main.py
+++ main.py
@@ -1,17 +1,48 @@
 import pygame
 from constants import *
-
-
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    x = SCREEN_WIDTH/2
+    y = SCREEN_HEIGHT/2
+
+    asteroid_group = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group() 
+
+    AsteroidField.containers = (updatable,)
+    Asteroid.containers = (asteroid_group, updatable, drawable)
+    Player.containers = (updatable, drawable) 
+    player = Player(x,y)
+    asteroidfield = AsteroidField()
+
+
+
+
+
+
     while True:
-         for event in pygame.event.get():
+        dt = clock.tick(60)/1000
+
+        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (0,0,0))
+        for obj in updatable:
+            obj.update(dt)
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
-        
+
+
 
 
 
